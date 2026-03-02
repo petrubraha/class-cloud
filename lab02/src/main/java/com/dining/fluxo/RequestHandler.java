593a9b57d7053002d98aa7023eabbfa070f6c980
P <petrubraha@gmail.com>
2026-03-02 21:04:08 +0200

    feat: modular functions for RequestHandler

--- lab02/src/main/java/com/dining/fluxo/RequestHandler.java
+++ lab02/src/main/java/com/dining/fluxo/RequestHandler.java
@@ -31,61 +31,13 @@ public class RequestHandler<R extends RestResource> implements HttpHandler {
 
         try {
             String method = exchange.getRequestMethod();
-            String uri = exchange.getRequestURI().getPath(); // Used getPath() to omit query params if any
+            String uri = exchange.getRequestURI().getPath();
+            String body = extractExangeBody(exchange.getRequestBody());
 
-            System.out.println("--- New Request ---");
-            System.out.println(method + " " + uri);
+            printRequestHead(method, uri, exchange.getRequestHeaders());
+            printRequestBody(body);
 
-            System.out.println("Headers:");
-            for (Map.Entry<String, List<String>> header : exchange.getRequestHeaders().entrySet()) {
-                System.out.println("  " + header.getKey() + ": " + String.join(", ", header.getValue()));
-            }
-
-            InputStream is = exchange.getRequestBody();
-            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-            int nRead;
-            byte[] data = new byte[1024];
-            while ((nRead = is.read(data, 0, data.length)) != -1) {
-                buffer.write(data, 0, nRead);
-            }
-            buffer.flush();
-            String body = new String(buffer.toByteArray(), StandardCharsets.UTF_8);
-
-            System.out.println("Payload: ");
-            System.out.println(body.isEmpty() ? "<empty>" : body);
-            System.out.println("-------------------");
-
-            RestResource resourceInstance = resourceClass.getDeclaredConstructor().newInstance();
-
-            Integer id = null;
-            String[] parts = uri.split("/");
-            if (parts.length > 2) {
-                try {
-                    id = Integer.parseInt(parts[2]);
-                } catch (NumberFormatException e) {
-                    throw new InvalidInputException("Invalid ID format in URI");
-                }
-            }
-
-            switch (method.toUpperCase()) {
-                case "GET":
-                    responseBytes = resourceInstance.resolveGet(id);
-                    break;
-                case "POST":
-                    if (id != null) {
-                        throw new ResourceNotFoundException("POST should be at root resource context, not on an ID");
-                    }
-                    responseBytes = resourceInstance.resolvePost(body);
-                    break;
-                case "PUT":
-                    responseBytes = resourceInstance.resolvePut(id, body);
-                    break;
-                case "DELETE":
-                    responseBytes = resourceInstance.resolveDelete(id);
-                    break;
-                default:
-                    throw new InvalidInputException("Unsupported method");
-            }
+            resolveRequest(method, uri, body);
 
         } catch (InvalidInputException e) {
             statusCode = 400;
@@ -114,11 +66,67 @@ public class RequestHandler<R extends RestResource> implements HttpHandler {
         }
     }
 
+    private String extractExangeBody(InputStream is) throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[1024];
+        while ((nRead = is.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+        buffer.flush();
+        return new String(buffer.toByteArray(), StandardCharsets.UTF_8);
+    }
+
+    private byte[] resolveRequest(String method, String uri, String body) throws IOException, Exception {
+        RestResource resourceInstance = resourceClass.getDeclaredConstructor().newInstance();
+
+        Integer id = null;
+        String[] parts = uri.split("/");
+        if (parts.length > 2) {
+            try {
+                id = Integer.parseInt(parts[2]);
+            } catch (NumberFormatException e) {
+                throw new InvalidInputException("Invalid ID format in URI");
+            }
+        }
+
+        switch (method.toUpperCase()) {
+            case "GET":
+                return resourceInstance.resolveGet(id);
+            case "POST":
+                if (id != null) {
+                    throw new ResourceNotFoundException("POST should be at root resource context, not on an ID");
+                }
+                return resourceInstance.resolvePost(body);
+            case "PUT":
+                return resourceInstance.resolvePut(id, body);
+            case "DELETE":
+                return resourceInstance.resolveDelete(id);
+            default:
+                throw new InvalidInputException("Unsupported method");
+        }
+    }
+
     private byte[] createErrorResponse(String error, String message) {
         try {
             return MAPPER.writeValueAsBytes(Map.of("error", error, "message", message));
         } catch (Exception e) {
             return "{\"error\":\"InternalServerError\"}".getBytes(StandardCharsets.UTF_8);
         }
+    }
+
+    private void printRequestHead(String method, String uri, Map<String, List<String>> headers) {
+        System.out.println("--- New Request ---");
+        System.out.printf("%s %s\n", method, uri);
+        System.out.println("Headers:");
+        for (Map.Entry<String, List<String>> header : headers.entrySet()) {
+            System.out.printf("  %s: %s\n", header.getKey(), String.join(", ", header.getValue()));
+        }
+    }
+
+    private void printRequestBody(String body) {
+        System.out.println("Payload: ");
+        System.out.println(body.isEmpty() ? "<empty>" : body);
+        System.out.println("-------------------");
     }
 }
